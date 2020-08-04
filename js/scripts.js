c15f9113db37b3c9fc9136d8d6acba57a24440aa
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2020-08-04 14:46:59 -0400

    filters

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,6 @@ main();
 ///// TEAM  API REQUEST ` `
 ////////////////////////////////////////////////////////////
 
-
 Vue.use(VueMeta);
 
 new Vue({
@@ -27,15 +26,49 @@ new Vue({
   el: '#home-page',
     
   data () {
-  
+ 
     return {
       indexData: [],
+      filterData: [],
+      js_project_types: [
+        { code: '', name: 'All' },
+        { code: 'collabs', name: 'Data Collaborative' },
+        { code: 'proposals-challenges', name: 'Data Challenges and Calls for Proposals' },
+        { code: 'requests', name: 'Requests for Data and Expertise' }
+      ],
+      js_regions: [
+        { code: '', name: 'All' },
+        { code: 'eap', name: 'East Asia and Pacific' },
+        { code: 'eca', name: 'Europe and Central Asia' },
+        { code: 'lac', name: 'Latin America and the Caribbean' },
+        { code: 'mena', name: 'Middle East and North Africa' },
+        { code: 'na', name: 'North America' },
+        { code: 'ssa', name: 'Sub-Saharan Africa' },
+        { code: 'global', name: 'Global' },
+      ],
+      js_areas: [
+        { code: '', name: 'All' },
+        { code: 'spread', name: 'Tracking Disease Spread' },
+        { code: 'treatment', name: 'Developing Disease Treatment' },
+        { code: 'supplies', name: 'Identifying Availability of Supplies' },
+        { code: 'aderence', name: 'Monitoring Adherence to Non-Pharmaceutical Interventions' },
+        { code: 'perceptions', name: 'Understanding Public Perceptions and Behavior' },
+        { code: 'accountability', name: 'Protecting Human Rights and Promoting Accountability' },
+        { code: 'misinformation', name: 'Addressing Misinformation' },
+        { code: 'recovery', name: 'Supporting Post-Pandemic Re-openings and Recovery' },
+        { code: 'unemployment', name: 'Alleviating Pandemic-related Unemployment and Poverty' },
+        { code: 'protections', name: 'Guaranteeing Protections for Workers' },
+        { code: 'upskilling', name: 'Supporting Education and Upskilling' },
+        { code: 'solvency', name: 'Fostering Business and Government Solvency' },
+      ],
+      selectedProjectType: null,
       apiURL: 'https://directus.thegovlab.com/data4covid',
     }
   },
 
   created: function created() {
     this.fetchIndex();
+
   },
   methods: {
 
@@ -53,10 +86,47 @@ new Vue({
     fields: ['*.*']
   }
 ).then(data => {
-  console.log(data)
+  
   self.indexData = data.data;
+  self.filterData = self.indexData;
 })
 .catch(error => console.error(error));
+    },
+    changeFilter (event) {
+      var element = document.body.querySelectorAll("select");
+      console.log(element[0].value);
+      console.log(element[1].value);
+      this.selectedProjectType = element[0].value;
+      this.selectedRegion = element[1].value;
+      if (this.selectedProjectType == '')
+      self.filtered_pt = self.indexData;
+      else{
+      let filtered_by_project_type = self.indexData.filter(function (e) {
+        console.log(e.project_type);
+        console.log(self.selectedProjectType);
+        console.log(e.project_type.some(pt_element => pt_element == self.selectedProjectType));
+        return e.project_type.some(pt_element => pt_element == self.selectedProjectType);
+    }
+    );
+    self.filtered_pt=filtered_by_project_type;
+    
+  }
+  if (this.selectedRegion == '')
+  self.filtered_region = self.filtered_pt;
+  else{
+    let filtered_by_region= self.filtered_pt.filter(function (e) {
+      console.log(e.region);
+      console.log(self.selectedRegion);
+      console.log(e.region.some(reg_element => reg_element == self.selectedRegion));
+      return e.region.some(reg_element => reg_element == self.selectedRegion);
+  }
+  
+  );
+  
+  self.filtered_region = filtered_by_region ;
+
+  }
+  self.filterData =  self.filtered_region;
     }
 }
 });
